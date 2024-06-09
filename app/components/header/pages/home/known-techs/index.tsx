1100f526f8a3728392b2462e5d1df3d82433a481
Thalles Daniel <106764514+ThallesDaniel@users.noreply.github.com>
2024-06-09 18:03:59 -0300

    adição de competencias

---
+++ app/components/header/pages/home/known-techs/index.tsx
@@ -0,0 +1,16 @@
+// type KnownTechsProps = {
+//   techs: IKnownTech[]
+// }
+
+import { SectionTitle } from "@/app/components/section-title"
+
+export const KnownTechs = () => {
+  return (
+    <section className="container py-16">
+        <SectionTitle subtitle="competências" title="Conhecimentos" />
+        <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
+
+        </div>
+    </section>
+  )
+  }
---
+++ app/components/section-title/index.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/app/lib/utils";
+
+
+type SectionTitleProps = {
+    subtitle: string
+    title: string
+    className?: string
+  }
+
+  export const SectionTitle = ({
+    subtitle,
+    title,
+    className,
+  }: SectionTitleProps) => {
+    return(
+        <div className={cn('flex flex-col gap-4', className)}>
+            <span className="font-mono text-sm text-emerald-400">
+                {`../${subtitle}`}
+            </span>
+            <h3 className="text-3xl font-medium">
+                {title}
+            </h3>
+        </div>
+    )
+  }
